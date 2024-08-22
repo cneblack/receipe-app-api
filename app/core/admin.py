8015900ab97ab00466b183c9125c9112651b606e
cneblack <cne.nero@outlook.com>
2024-08-22 15:20:31 +0200

    Fixing linting

--- app/core/admin.py
+++ app/core/admin.py
@@ -6,6 +6,7 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from core import models
 from django.utils.translation import gettext_lazy as _
 
+
 class UserAdmin(BaseUserAdmin):
     """Define the admin pages for users."""
     ordering = ['id']

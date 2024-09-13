a84196e51b5e78f098cd0cf318c0f587e078ec98
cneblack <cne.nero@outlook.com>
2024-09-13 17:08:18 +0200

    Updated django settings

--- app/app/settings.py
+++ app/app/settings.py
@@ -24,12 +24,18 @@ VERSION = "0.0.1"
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o(zkw2ov&64)@xara2_eu4rlvtepl%n^vmrl-d8heio!lm!v2^'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
 ALLOWED_HOSTS = []
+ALLOWED_HOSTS.extend(
+    filter(
+        None,
+        os.environ.get('ALLOWED_HOSTS', '').split(','),
+    )
+)
 
 
 # Application definition
